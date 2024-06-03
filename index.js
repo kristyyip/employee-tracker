67292aac3eb064d77480db3b91ed46a830bafc39
kristyyip <kristyyip51998@gmail.com>
2024-06-03 00:54:41 -0400

    create showPrompt function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const pgp = require("pg-promise");
 const inquirer = require("inquirer");
 
+// conditional prompt depending on choice
+// src: https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
 const questions = [
     {
     type: "list",
     message: "What would you like to do?",
-    name: "activity",
+    name: "option",
     choices: ["View all employees", 
         "Add employee", 
         "Update employee role", 
@@ -15,11 +17,12 @@ const questions = [
         "Add department",
         "Quit"
     ]},
+    // questions to ask if Add employee is selected
     {
         type: "input",
         message: "What is the employee's first name?",
         name: "firstName",
-        when: (response) => response.activity === "Add employee"
+        when: (response) => response.option === "Add employee"
     },
     {
         type: "input",
@@ -39,11 +42,12 @@ const questions = [
         name: "employeeManager",
         when: (response) => response.employeeRole
     },
+    // questions to ask if Add role is selected
     {
         type: "input",
         message: "What is the name of the role?",
         name: "role",
-        when: (response) => response.activity === "Add role"
+        when: (response) => response.option === "Add role"
     },
     {
         type: "input",
@@ -57,19 +61,46 @@ const questions = [
         name: "dept",
         when: (response) => response.roleSalary
     },
+    // question to ask if Add department is selected
     {
         type: "input",
         message: "What is the name of the department?",
         name: "dept",
-        when: (response) => response.activity === "Add department"
+        when: (response) => response.option === "Add department"
     }
 ]
 
-
-// conditional prompt depending on choice
-// src: https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
-inquirer
+// function to show inquirer prompt to user
+const showPrompt = (questions) => {
+    inquirer
     .prompt(questions)
     .then((response) => {
-    }
-    );
+        // if user selects Quit, exit out of inquirer promopt
+        if (response.option === "Quit") {
+            process.exit();
+        }
+        // otherwise, show, add to, or update table depending on selection
+        else {
+            if (response.option === "View all employees") {
+                
+            } else if (response.option === "Add employee") {
+
+            } else if (response.option === "Update employee role") {
+
+            } else if (response.option === "View all roles") {
+
+            } else if (response.option === "Add role") {
+                
+            } else if (response.option === "View all departments") {
+                
+            } else {
+
+            }
+            
+            // show prompt again using recursion
+            showPrompt(questions);
+        }
+    })
+}
+
+showPrompt(questions);
