b20dae14910a04c5a62ed3648bf2d3263985b8f5
kristyyip <kristyyip51998@gmail.com>
2024-07-12 10:01:10 -0400

    added code for add  employee prompt

--- db/index.js
+++ db/index.js
@@ -1,26 +1,3 @@
-const { Console } = require('console');
-const { Transform } = require('stream');
-
-// remove (index) column from console.table
-// src: https://stackoverflow.com/questions/49618069/remove-index-from-console-table
-function table(input) {
-  // @see https://stackoverflow.com/a/67859384
-  const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
-  const logger = new Console({ stdout: ts })
-  logger.table(input)
-  const table = (ts.read() || '').toString()
-  let result = '';
-  for (let row of table.split(/[\r\n]+/)) {
-    let r = row.replace(/[^┬]*┬/, '┌');
-    r = r.replace(/^├─*┼/, '├');
-    r = r.replace(/│[^│]*/, '');
-    r = r.replace(/^└─*┴/, '└');
-    r = r.replace(/'/g, ' ');
-    result += `${r}\n`;
-  }
-  console.log(`\n${result}`);
-}
-
 const { Pool } = require("pg");
 
 const pool = new Pool (
@@ -37,23 +14,39 @@ pool.connect();
 
 // function to view all employees
 function findEmployees() {
-  pool.query("SELECT e1.id, e1.first_name, e1.last_name, role.title, department.name as department, salary, CONCAT(e2.first_name, ' ', e2.last_name) as manager FROM employee AS e1 LEFT JOIN role ON e1.role_id = role.id LEFT JOIN department ON role.department = department.id LEFT JOIN employee AS e2 on e1.manager_id = e2.id;", (err, {rows}) => {
-    return table(rows);
-  })
+  return pool.query("SELECT e1.id, e1.first_name, e1.last_name, role.title, department.name as department, salary, CONCAT(e2.first_name, ' ', e2.last_name) as manager FROM employee AS e1 LEFT JOIN role ON e1.role_id = role.id LEFT JOIN department ON role.department = department.id LEFT JOIN employee AS e2 on e1.manager_id = e2.id;");
+}
+
+function findDeptEmployees(departmentID) {
+  return pool.query("SELECT e.id, first_name, last_name, department FROM employee as e LEFT JOIN role on e.role_id = role.id WHERE department = $1;", [departmentID])
 }
 
 // function to view all roles
 function findRoles() {
-  pool.query("SELECT role.id, title, department.name as department, salary FROM role JOIN department ON role.department = department.id", (err, {rows}) => {
-    return table(rows);
-  })
+  return pool.query("SELECT role.id, title, department.name as department, salary FROM role JOIN department ON role.department = department.id");
+}
+
+function findDeptRole(roleID) {
+  return pool.query("SELECT department FROM role WHERE id = $1;", [roleID]);
 }
 
 // function to view all departments
 function findDepartments() {
-  pool.query("SELECT * FROM department", (err, {rows}) => {
-    return table(rows);
-  })
+  return pool.query("SELECT * FROM department");
 }
 
-module.exports = {findEmployees, findRoles, findDepartments}
+function addEmployee(firstName, lastName, employeeRole, employeeManager) {
+  return pool.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4);", [firstName, lastName, employeeRole, employeeManager])
+}
+
+function addRole(title, salary, deptName) {
+  return pool.query("INSERT INTO role (title, salary, department) VALUES ($1, $2, $3)", [title, salary, deptName])
+}
+
+function addDepartment(dept) {
+  return pool.query("INSERT INTO department (name) VALUES ($1);", [dept]);
+
+  // console.log(`Added ${dept} to the database`)
+}
+
+module.exports = {findEmployees, findDeptEmployees, findRoles, findDeptRole, findDepartments, addEmployee, addRole, addDepartment}
--- index.js
+++ index.js
@@ -1,6 +1,126 @@
 const pg = require("pg");
 const inquirer = require("inquirer");
-const {findEmployees, findRoles, findDepartments} = require("./db")
+const { Console } = require('console');
+const { Transform } = require('stream');
+const {findEmployees, findDeptEmployees, findRoles, findDeptRole, findDepartments, addEmployee, addRole, addDepartment} = require("./db");
+const { create } = require("domain");
+
+// remove (index) column from console.table
+// src: https://stackoverflow.com/questions/49618069/remove-index-from-console-table
+function table(input) {
+    // @see https://stackoverflow.com/a/67859384
+    const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+    const logger = new Console({ stdout: ts })
+    logger.table(input)
+    const table = (ts.read() || '').toString()
+    let result = '';
+    for (let row of table.split(/[\r\n]+/)) {
+      let r = row.replace(/[^┬]*┬/, '┌');
+      r = r.replace(/^├─*┼/, '├');
+      r = r.replace(/│[^│]*/, '');
+      r = r.replace(/^└─*┴/, '└');
+      r = r.replace(/'/g, ' ');
+      result += `${r}\n`;
+    }
+    console.log(`\n${result}`);
+  }
+
+  async function createRoleChoices() {
+    const {rows} = await findRoles();
+    const roleChoices = rows.map(({id, title}) => 
+        ({
+            value: id, 
+            name: title,
+        })
+    );
+
+    return roleChoices;
+}
+
+async function createEmployeeChoices() {
+    const {rows} = await findEmployees();
+    const employeeChoices = rows.map(({id, first_name, last_name}) => 
+        ({
+            value: id, 
+            name: `${first_name} ${last_name}`,
+        })
+    );
+
+    return employeeChoices;
+}
+
+async function findDeptID(roleID) {
+    const {rows} = await findDeptRole(roleID);
+    return rows[0].department;
+}
+
+async function createManagerChoices(roleID) {
+    const deptID = await findDeptID(roleID);
+
+    const {rows} = await findDeptEmployees(deptID);
+    const managerChoices = rows.map(({id, first_name, last_name}) => 
+        ({
+            value: id, 
+            name: `${first_name} ${last_name}`,
+        })
+    );
+
+    return managerChoices;
+}
+
+async function addEmployeePrompt() {
+    let roleChoices = await createRoleChoices();
+    
+    await inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "firstName",
+        },
+        {
+            type: "input",
+            message: "What is the employee's last name name?",
+            name: "lastName",
+            when: (response) => response.firstName
+        },
+        {
+            type: "list",
+            message: "What is the employee's role?",
+            name: "employeeRole",
+            choices: roleChoices,
+            when: (response) => response.lastName
+        },
+    ])
+    .then(async (response) => {
+        let managerChoices = await createManagerChoices(response.employeeRole);
+
+        await inquirer
+        .prompt([
+        {
+            type: "list",
+            message: "Who's the employee's manager?",
+            choices: managerChoices,
+            name: "employeeManager"
+        },])
+        .then((res) => {
+            addEmployee(response.firstName, response.lastName, response.employeeRole, res.employeeManager);
+        })
+    })
+
+}
+
+async function createDeptChoices() {
+    const {rows} = await findDepartments();
+    const deptChoices = rows.map(({id, name}) => 
+        ({
+            value: id, 
+            name: name,
+        })
+    );
+
+    return deptChoices;
+}
 
 // conditional prompt depending on choice
 // src: https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
@@ -19,69 +139,13 @@ const questions = [
         "Quit"
     ]},
     // questions to ask if Add employee is selected
-    {
-        type: "input",
-        message: "What is the employee's first name?",
-        name: "firstName",
-        when: (response) => response.option === "Add employee"
-    },
-    {
-        type: "input",
-        message: "What is the employee's last name name?",
-        name: "lastName",
-        when: (response) => response.firstName
-    },
-    {
-        type: "input",
-        message: "What is the employee's role?",
-        name: "employeeRole",
-        when: (response) => response.lastName
-    },
-    {
-        type: "input",
-        message: "Who's the employee's manager?",
-        name: "employeeManager",
-        when: (response) => response.employeeRole
-    },
+    
     // questions to ask if Update employee role is selected
-    {
-        type: "input",
-        message: "Which employee's role do you want to update?",
-        name: "employee",
-        when: (response) => response.option === "Update employee role"
-    },
-    {
-        type: "input",
-        message: "Which role do you want to assign the selected employee?",
-        name: "newRole",
-        when: (response) => response.employee
-    },
+    
     // questions to ask if Add role is selected
-    {
-        type: "input",
-        message: "What is the name of the role?",
-        name: "role",
-        when: (response) => response.option === "Add role"
-    },
-    {
-        type: "input",
-        message: "What is the salary of the role?",
-        name: "roleSalary",
-        when: (response) => response.role
-    },
-    {
-        type: "input",
-        message: "Which department does the role belong to?",
-        name: "dept",
-        when: (response) => response.roleSalary
-    },
+    
     // question to ask if Add department is selected
-    {
-        type: "input",
-        message: "What is the name of the department?",
-        name: "dept",
-        when: (response) => response.option === "Add department"
-    }
+   
 ]
 
 // function to show inquirer prompt to user
@@ -96,25 +160,58 @@ const showPrompt = (questions) => {
         // otherwise, show, add to, or update table depending on selection
         else {
             if (response.option === "View all employees") {
-                findEmployees();
+                findEmployees()
+                    .then(({rows}) => table(rows));
             } else if (response.option === "Add employee") {
-
+                addEmployeePrompt();
             } else if (response.option === "Update employee role") {
-
+                // {
+                //     type: "input",
+                //     message: "Which employee's role do you want to update?",
+                //     name: "employee",
+                //     when: (response) => response.option === "Update employee role"
+                // },
+                // {
+                //     type: "input",
+                //     message: "Which role do you want to assign the selected employee?",
+                //     name: "newRole",
+                //     when: (response) => response.employee
+                // },
             } else if (response.option === "View all roles") {
-                findRoles();
+                findRoles()
+                    .then(({rows}) => table(rows));
             } else if (response.option === "Add role") {
-                
+                // {
+                //     type: "input",
+                //     message: "What is the name of the role?",
+                //     name: "title",
+                //     when: (response) => response.option === "Add role"
+                // },
+                // {
+                //     type: "input",
+                //     message: "What is the salary of the role?",
+                //     name: "salary",
+                //     when: (response) => response.title
+                // },
+                // {
+                //     type: "input",
+                //     message: "Which department does the role belong to?",
+                //     name: "deptName",
+                //     // choices: deptChoices,
+                //     when: (response) => response.salary
+                // },
             } else if (response.option === "View all departments") {
-                findDepartments();
+                findDepartments()
+                    .then(({rows}) => table(rows));
             } else {
-
+                // {
+                //     type: "input",
+                //     message: "What is the name of the department?",
+                //     name: "dept",
+                //     when: (response) => response.option === "Add department"
+                // }
             }
-            
-            // show prompt again using recursion
-            showPrompt(questions);
-        }
-    })
+    }})
 }
 
 showPrompt(questions);
